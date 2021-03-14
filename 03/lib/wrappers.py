1593eb83d89c86e5678ebf06fe515307d923d143
Muhammad Isfaaq Goomany <isfaaqg@gmail.com>
2021-03-14 10:40:33 +0400

    update wrapper

--- 03/lib/wrappers.py
+++ 03/lib/wrappers.py
@@ -52,6 +52,37 @@ class MaxAndSkipEnv(gym.Wrapper):
         obs = self.env.reset()
         self._obs_buffer.append(obs)
         return obs
+    
+
+class NoopResetEnv(gym.Wrapper):
+    def __init__(self, env, noop_max=30):
+        """Sample initial states by taking random number of no-ops on reset.
+        No-op is assumed to be action 0.
+        """
+        gym.Wrapper.__init__(self, env)
+        self.noop_max = noop_max
+        self.override_num_noops = None
+        self.noop_action = 0
+        assert env.unwrapped.get_action_meanings()[0] == 'NOOP'
+
+    def reset(self, **kwargs):
+        """ Do no-op action for a number of steps in [1, noop_max]."""
+        self.env.reset(**kwargs)
+        if self.override_num_noops is not None:
+            noops = self.override_num_noops
+        else:
+            noops = self.unwrapped.np_random.randint(
+                1, self.noop_max + 1)  # pylint: disable=E1101
+        assert noops > 0
+        obs = None
+        for _ in range(noops):
+            obs, _, done, _ = self.env.step(self.noop_action)
+            if done:
+                obs = self.env.reset(**kwargs)
+        return obs
+
+    def step(self, ac):
+        return self.env.step(ac)
 
 
 class ProcessFrame84(gym.ObservationWrapper):
@@ -231,6 +262,7 @@ def make_env_bo_rot(env_name):
     env = gym.make(env_name)
     env = EpisodicLifeEnv(env)
     env = ClipRewardEnv(env)
+    env = NoopResetEnv(env)
     env = MaxAndSkipEnv(env)
     env = FireResetEnv(env)
     env = ProcessFrame84BreakoutRotate(env)
--- README.md
+++ README.md
@@ -30,6 +30,35 @@ From 02
 
 [ ] Apply Custom Breakout processing to Rotate (Breakout must be rotated by 90cw)
 
+> 12548: done 474 games, mean reward 0.090, eps 0.87, speed 9.45 f/s max reward 0.3
+
+```
+    env = gym.make(env_name)
+    env = EpisodicLifeEnv(env)
+    env = ClipRewardEnv(env)
+    env = MaxAndSkipEnv(env)
+    env = FireResetEnv(env)
+    env = ProcessFrame84BreakoutRotate(env)
+    env = ImageToPyTorch(env)
+    env = BufferWrapper(env, 4)
+    env = ScaledFloatFrame(env)
+```
+
+> 12257: done 354 games, mean reward 0.340, eps 0.88, speed 11.56 f/s max reward 0.333
+
+```
+    env = gym.make(env_name)
+    env = EpisodicLifeEnv(env)
+    env = ClipRewardEnv(env)
+    env = NoopResetEnv(env)
+    env = MaxAndSkipEnv(env)
+    env = FireResetEnv(env)
+    env = ProcessFrame84BreakoutRotate(env)
+    env = ImageToPyTorch(env)
+    env = BufferWrapper(env, 4)
+    env = ScaledFloatFrame(env)
+```
+
 [ ] Train till score = 8 and save model
 
 [ ] Load model and train till score = 12
